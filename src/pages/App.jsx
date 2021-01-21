4e1c9e6eccc9ff5b41ff404ca7742b5d2c33cd13
wangchao <wangc5@kingnet.com>
2021-01-21 16:02:57 +0800

    remove video.js

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
 import "./app.css";
-import imgsrc from "../assets/img/tip.png";
 import Button from "./components/button.jsx";
 import ButtonGroup from "./components/ButtonGroup.jsx";
-import Movie from "./movie";
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -25,7 +23,6 @@ class App extends React.Component {
         return (
             <HashRouter>
                 <Route path="/" component={ButtonGroup} />
-                <Route path="/player" component={Movie} />
                 {/* <Route path="/" component={ButtonGroup} />
                 <Route path="/" component={ButtonGroup} /> */}
                 <h1>app title12323</h1>
